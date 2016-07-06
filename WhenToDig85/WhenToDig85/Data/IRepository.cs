d9386529705896d14c2a764d67c1ecb3d4906561
ivNetAdmin <bp@ivnet.co.uk>
2016-07-06 11:57:09 +0100

    Create IRepository.cs
    Repository set up

---
+++ WhenToDig85/WhenToDig85/Data/IRepository.cs
@@ -0,0 +1,20 @@
+using SQLite.Net.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WhenToDig85.Data
+{   
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> Get();
+        Task<T> Get(int id);
+        Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null);
+        Task<T> Get(Expression<Func<T, bool>> predicate);
+        AsyncTableQuery<T> AsQueryable();
+        Task<int> Insert(T entity);
+        Task<int> Update(T entity);
+        Task<int> Delete(T entity);
+    }
+}
