ab655123555ae478bac64f5fe26756c087ee51b8
ivNetAdmin <bp@ivnet.co.uk>
2016-07-06 11:59:01 +0100

    Update Repository.cs
    Repository set up

--- WhenToDig85/WhenToDig85/Data/Repository.cs
+++ WhenToDig85/WhenToDig85/Data/Repository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using SQLite.Net.Async;
 
-namespace WhenToDig90.Data
+namespace WhenToDig85.Data
 {
     public class Repository<T> : IRepository<T> where T : class
     {
