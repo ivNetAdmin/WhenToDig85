590529c9dbf6441da05b94a41cf3fe04424f8110
ivNetAdmin <bp@ivnet.co.uk>
2016-07-14 12:03:32 +0100

    Update VarietyView.xaml.cs
    Variety view

--- WhenToDig85/WhenToDig85/Views/VarietyView.xaml.cs
+++ WhenToDig85/WhenToDig85/Views/VarietyView.xaml.cs
@@ -10,9 +10,35 @@ namespace WhenToDig85.Views
 {
     public partial class VarietyView : ContentPage
     {
+        private VarietyViewModel _viewModel;
         public VarietyView()
         {
             InitializeComponent();
+            BindingContext = App.Locator.VarietyVM;
+            
+            _viewModel.ClearFormCallBackAction = () => ClearForm();
+            _viewModel.UserErrorMessageCallBackAction = () => DisplayUserErrorMessage(_viewModel.UserMessage);
+        }
+        
+        protected override void OnAppearing()
+        {
+            ClearForm();
+
+            Context.OnAppearing();
+            base.OnAppearing();
+        }
+        
+        private IPageLifeCycleEvents Context
+        {
+            get { return (IPageLifeCycleEvents)BindingContext; }
+        }
+        
+        private void ClearForm()
+        {
+        }
+        
+        private void DisplayUserErrorMessage(string userMessage)
+        {
         }
     }
 }
