7ec30c3bb32458196071cea5301fe6de3c51dfab
ivNetAdmin <bp@ivnet.co.uk>
2016-07-15 11:02:35 +0100

    Update Variety.cs
    Variety

--- WhenToDig85/WhenToDig85/Models/Data/Variety.cs
+++ WhenToDig85/WhenToDig85/Models/Data/Variety.cs
@@ -1,1 +1,16 @@
 
+using SQLite.Net.Attributes;
+
+namespace WhenToDig85.Models.Data
+{
+    public class Variety : Base
+    {
+        [NotNull]
+        public string Name { get; set; }
+        public string PlantName { get; set; 
+        public string SowNotes { get; set; }
+        public string HarvestNotes { get; set; }
+
+        public Variety() { }
+    }
+}
